d8b566d0d0bcb610ae48f250e5da9b9a49a5581a
adrielcorreia <adrielcostacorreia54@gmail.com>
2023-09-20 11:09:18 -0300

    all-done

--- js/main.js
+++ js/main.js
@@ -3,14 +3,22 @@ let ham_active = false;
 const ham_button = document.getElementById('ham')
 const header_menu = document.querySelector(".header-menu");
 
-ham_button.addEventListener('click', () => {
+ham_button.onmouseover = (event) => {
     if (ham_active == false) {
+        ham_button.classList.toggle('openmenu');
+
         header_menu.style.visibility = "visible";
         header_menu.style.transform = "translateY(0)";
         ham_active = true;
-    } else {
+    }
+}
+
+ham_button.onmouseleave = (event) => {
+    if (ham_active == true) {
+        ham_button.classList.toggle('openmenu');
+
         header_menu.style.visibility = "hidden";
         header_menu.style.transform = "translateY(-100%)";
         ham_active = false; 
     }
-});
+}
