e4ca14fcb5a4502caea83dec163830383befa28b
adrielcorreia <adrielcostacorreia54@gmail.com>
2023-10-01 18:19:39 -0300

    header-alteration

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@ let ham_active = false;
 
 const ham_button = document.getElementById('ham')
 const header_menu = document.querySelector(".header-menu");
+const ul = document.querySelector('#menu-ul')
+const ullHeight = getComputedStyle(ul).height;
+console.log(ullHeight)
 
 ham_button.onmouseover = (event) => {
     if (ham_active == false) {
         ham_button.classList.toggle('openmenu');
 
         header_menu.style.visibility = "visible";
-        header_menu.style.transform = "translateY(0)";
+        header_menu.style.height = ullHeight;
         ham_active = true;
     }
 }
@@ -18,7 +21,7 @@ ham_button.onmouseleave = (event) => {
         ham_button.classList.toggle('openmenu');
 
         header_menu.style.visibility = "hidden";
-        header_menu.style.transform = "translateY(-100%)";
+        header_menu.style.height = "0";
         ham_active = false; 
     }
 }
